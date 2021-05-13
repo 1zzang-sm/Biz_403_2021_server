ddbb868afa5cefd67e6ab831db61848ffa5e3e1d
1zzang-sm <captain1152@naver.com>
2021-05-13 12:23:02 +0900

    05.13

--- Server_09_Diet/src/main/java/com/callor/diet/service/impl/FoodServiceImplV1.java
+++ Server_09_Diet/src/main/java/com/callor/diet/service/impl/FoodServiceImplV1.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jdt.internal.compiler.batch.FileSystem.ClasspathSectionProblemReporter;
+
 import com.callor.diet.config.DBContract;
 import com.callor.diet.config.DBInfo;
 import com.callor.diet.model.FoodDTO;
@@ -14,25 +16,23 @@ import com.callor.diet.model.FoodVO;
 import com.callor.diet.service.FoodService;
 
 public class FoodServiceImplV1 implements FoodService {
-	
+
 	protected Connection dbConn;
-	
+
 	public FoodServiceImplV1() {
-		
 		dbConn = DBContract.getDBConnection();
 	}
-	
+
 	// DBMS에 SQL을 보내고
 	// 결과를 받아서 List 데이터로 만들어 주는 함수
-	protected List<FoodDTO> select(PreparedStatement pStr) throws SQLException{
-		
+	protected List<FoodDTO> select(PreparedStatement pStr) throws SQLException {
+
 		ResultSet rSet = pStr.executeQuery();
-		
-		List<FoodDTO> foodList = new ArrayList<>();
-		
-		// DBMS에서 받은 데이터가 있으면 
-		while(rSet.next()) {
-			
+		List<FoodDTO> foodList = new ArrayList<FoodDTO>();
+
+		// DBMS에서 받은 데이터가 있으면
+		while (rSet.next()) {
+
 			FoodDTO dto = new FoodDTO();
 			dto.setFd_code(rSet.getString(DBInfo.FOOD.fd_code));
 			dto.setFd_name(rSet.getString(DBInfo.FOOD.fd_name));
@@ -46,18 +46,19 @@ public class FoodServiceImplV1 implements FoodService {
 			dto.setFd_fat(rSet.getFloat(DBInfo.FOOD.fd_fat));
 			dto.setFd_carbo(rSet.getFloat(DBInfo.FOOD.fd_carbo));
 			dto.setFd_sugar(rSet.getFloat(DBInfo.FOOD.fd_sugar));
+
 			dto.setCp_name(rSet.getString(DBInfo.FOOD.cp_name));
 			dto.setCp_ceo(rSet.getString(DBInfo.FOOD.cp_ceo));
 			dto.setCp_tel(rSet.getString(DBInfo.FOOD.cp_tel));
 			dto.setCp_addr(rSet.getString(DBInfo.FOOD.cp_addr));
 			dto.setCp_item(rSet.getString(DBInfo.FOOD.cp_item));
+
 			dto.setIt_name(rSet.getString(DBInfo.FOOD.it_name));
 			foodList.add(dto);
 		}
-		rSet.close();
 		return foodList;
 	}
-	
+
 	@Override
 	public List<FoodDTO> selectAll() {
 		// TODO Auto-generated method stub
@@ -73,14 +74,14 @@ public class FoodServiceImplV1 implements FoodService {
 	@Override
 	public List<FoodDTO> findByFName(String fd_name) {
 		// TODO 식품명으로 검색하기
-		String sql = " SELECT * FROM view_식품정보 ";
+		String sql = "SELECT * FROM view_식품정보 ";
 		sql += " WHERE 식품명 LIKE '%' || ? || '%' ";
 		
 		PreparedStatement pStr = null;
-		
 		try {
+			
 			pStr = dbConn.prepareStatement(sql);
-			pStr.setString(1, fd_name.trim());
+			pStr.setString(1, fd_name.trim() );
 			List<FoodDTO> foodList = this.select(pStr);
 			pStr.close();
 			return foodList;
@@ -89,6 +90,8 @@ public class FoodServiceImplV1 implements FoodService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 		return null;
 	}
 
@@ -109,6 +112,5 @@ public class FoodServiceImplV1 implements FoodService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 
 }
