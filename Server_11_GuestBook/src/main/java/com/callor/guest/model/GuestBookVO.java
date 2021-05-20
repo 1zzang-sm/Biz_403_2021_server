17c8f249ccf3b48de9f7dfbb7f56a00b45d27991
1zzang-sm <captain1152@naver.com>
2021-05-20 16:05:30 +0900

    05.20

---
+++ Server_11_GuestBook/src/main/java/com/callor/guest/model/GuestBookVO.java
@@ -0,0 +1,23 @@
+package com.callor.guest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class GuestBookVO {
+
+	private Long gb_seq;
+	private String gb_date;
+	private String gb_time;
+	private String gb_wirter;
+	private String gb_email;
+	private String gb_password;
+	private String gb_content;
+}
