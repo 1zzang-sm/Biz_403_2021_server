beb34e53d55b87a87abfbf053b8bc9f750da992b
1zzang-sm <captain1152@naver.com>
2021-05-26 16:53:51 +0900

    05.26

--- Server_013_TodoList/src/main/java/com/callor/todo/controller/FrontController.java
+++ Server_013_TodoList/src/main/java/com/callor/todo/controller/FrontController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.callor.todo.command.HomeCommandImplV1;
 import com.callor.todo.command.TodoCommand;
+import com.callor.todo.command.TodoCommandImplV1;
 @WebServlet("/")
 public class FrontController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
@@ -23,8 +24,25 @@ public class FrontController extends HttpServlet{
 	// 여러가지 변수 등을 초기화 하는 코드
 	@Override
 	public void init(ServletConfig config) throws ServletException {
+		
+		System.out.println("init");
 		commands = new HashMap<String, TodoCommand>();
+		
+		/*
+		 *  만약 http://localhost:8080/todo 로 요청이 오면
+		 *  HomeCommandImplV1 객체를 사용하여
+		 *  요청을 처리하기 위한 준비
+		 */
+		
 		commands.put("/", new HomeCommandImplV1());
+		
+		/*
+		 * 만약 http://localhost:8080/todo/insert로 
+		 * 요청이 오면
+		 * TodoCommandImplV1 객체를 사용하여
+		 * 요청을 처리하기 위한 준비 
+		 */
+		commands.put("/insert", new TodoCommandImplV1());
 		
 	}
 	
@@ -33,13 +51,21 @@ public class FrontController extends HttpServlet{
 	// 한개의 method에서 동시에 처리하기
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		
+		req.setCharacterEncoding("UTF-8");
 		// web에서 요청한 path 가져오기
 		String urlPath = req.getRequestURI();
 		String path = urlPath.substring(req.getContextPath().length());
 		
+		
+		System.out.println(path);
+		// req 된 URI 중에서
+		// 실제 usbPath 부분을 사용하여
+		// 처리할 객체를 Map으로 부터 추출
 		TodoCommand subCommand = commands.get(path);
 		if(subCommand != null) {
+			
+			//각 Command 객체의 execute() method에게
+			// 실제 요청을 처리하도록 위임하기
 			subCommand.execute(req, res);
 		}
 		
